32d641bafd774520e3451e8c1bf761d22f1905c2
IrinaBBB <balkarovairina@gmail.com>
2023-07-02 17:24:13 +0200

    Initial commit

---
+++ PPMToolSpring/src/main/java/ru/aurorahost/ppmtool/PpmToolSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.aurorahost.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolSpringApplication.class, args);
+    }
+
+}
