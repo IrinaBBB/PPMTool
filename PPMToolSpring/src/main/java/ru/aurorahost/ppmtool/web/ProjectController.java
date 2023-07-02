000415b55e552cdcb923c8d3d1097d414ceb62f1
IrinaBBB <balkarovairina@gmail.com>
2023-07-02 21:05:14 +0200

    Project Object Validation part 2

--- PPMToolSpring/src/main/java/ru/aurorahost/ppmtool/web/ProjectController.java
+++ PPMToolSpring/src/main/java/ru/aurorahost/ppmtool/web/ProjectController.java
@@ -4,11 +4,13 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.aurorahost.ppmtool.domain.Project;
 import ru.aurorahost.ppmtool.service.ProjectService;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,7 +25,11 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError fieldError : result.getFieldErrors()) {
+                errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
         Project savedProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
