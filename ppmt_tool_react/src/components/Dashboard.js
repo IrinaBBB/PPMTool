a72b333e449b37d4c144dcbd6492aff38c1c272d
IrinaBBB <balkarovairina@gmail.com>
2023-07-05 20:30:28 +0200

    Style our Dashboard, Navbar, ProjectItem

--- ppmt_tool_react/src/components/Dashboard.js
+++ ppmt_tool_react/src/components/Dashboard.js
@@ -5,8 +5,31 @@ class Dashboard extends Component {
     render() {
         return (
             <>
-                <h1>Welcome to Dashboard</h1>
-                <ProjectItem />
+                <div className="projects">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1 className="text-center h2">
+                                    <i className="fa fa-flag-checkered pr-1">
+                                        &nbsp;Projects
+                                    </i>
+                                </h1>
+                                <br />
+                                <a
+                                    href="ProjectForm.html"
+                                    className="btn btn-lg btn-info"
+                                >
+                                    <i className="fa fa-plus-circle pr-1">
+                                    </i>
+                                    &nbsp;Create a Project
+                                </a>
+                                <br />
+                                <hr />
+                                <ProjectItem />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
--- ppmt_tool_react/src/components/Layout/Header.js
+++ ppmt_tool_react/src/components/Layout/Header.js
@@ -4,43 +4,37 @@ class Header extends Component {
     render() {
         return (
             <>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+                    <div className="container">
+                        <a className="navbar-brand" href="Dashboard.html">
+                            Personal Project Management Tool
+                        </a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+                            <span className="navbar-toggler-icon"/>
+                        </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                   aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <div className="dropdown-menu">
-                                    <a className="dropdown-item" href="#">Action</a>
-                                    <a className="dropdown-item" href="#">Another action</a>
-                                    <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                        <form className="form-inline my-2 my-lg-0">
-                            <input className="form-control mr-sm-2" type="search" placeholder="Search"
-                                   aria-label="Search" />
-                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
+                        <div className="collapse navbar-collapse" id="mobile-nav">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/dashboard">
+                                        Dashboard
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link " href="register.html">
+                                        Sign Up
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="login.html">
+                                        Login
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </>
