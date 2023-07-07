bc28221ad3f1f5a0970fa2915ce3604860b11057
IrinaBBB <balkarovairina@gmail.com>
2023-07-07 09:24:23 +0200

    Refactor Delete Operation and Proxy

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import {DELETE_PROJECT, GET_ERRORS, GET_PROJECT, GET_PROJECTS} from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
     try {
-        const res = await axios.post("http://localhost:8082/api/project", project);
+        const res = await axios.post("/api/project", project);
         history.push("/dashboard");
         dispatch({
             type: GET_ERRORS,
             payload: {}
-        })
+        });
     } catch (err) {
         dispatch({
             type: GET_ERRORS,
@@ -18,16 +18,16 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-    const res = await axios.get("http://localhost:8082/api/project/all");
+    const res = await axios.get("/api/project/all");
     dispatch({
         type: GET_PROJECTS,
         payload: res.data
-    })
-}
+    });
+};
 
 export const getProject = (id, history) => async dispatch => {
     try {
-        const res = await axios.get(`http://localhost:8082/api/project/${id}`);
+        const res = await axios.get(`/api/project/${id}`);
         dispatch({
             type: GET_PROJECT,
             payload: res.data
@@ -35,12 +35,18 @@ export const getProject = (id, history) => async dispatch => {
     } catch (error) {
         history.push("/dashboard");
     }
-}
+};
 
 export const deleteProject = id => async dispatch => {
-    await axios.delete(`http://localhost:8082/api/project/${id}`);
-    dispatch({
-        type: DELETE_PROJECT,
-        payload: id
-    });
+    if (
+        window.confirm(
+            "Are you sure? This will delete the project and all the data related to it"
+        )
+    ) {
+        await axios.delete(`/api/project/${id}`);
+        dispatch({
+            type: DELETE_PROJECT,
+            payload: id
+        });
+    }
 };
