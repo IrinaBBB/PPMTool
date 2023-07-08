5dbe39a0e38b60eb3419fb4e5cc41586cba4ab66
IrinaBBB <balkarovairina@gmail.com>
2023-07-08 09:50:37 +0200

    Finish up with update validation and delete

--- PPMToolSpring/src/main/java/ru/aurorahost/ppmtool/service/ProjectTaskService.java
+++ PPMToolSpring/src/main/java/ru/aurorahost/ppmtool/service/ProjectTaskService.java
@@ -9,6 +9,7 @@ import ru.aurorahost.ppmtool.repository.BacklogRepository;
 import ru.aurorahost.ppmtool.repository.ProjectRepository;
 import ru.aurorahost.ppmtool.repository.ProjectTaskRepository;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -75,8 +76,22 @@ public class ProjectTaskService {
     }
 
     public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id) {
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
         projectTask = updatedTask;
+
         return projectTaskRepository.save(projectTask);
+    }
+
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id) {
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+
+        projectTaskRepository.delete(projectTask);
     }
 }
