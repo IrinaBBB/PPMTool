c2f8f2cab9ab1eb69271d6f60ca2fff036137bd0
IrinaBBB <balkarovairina@gmail.com>
2023-07-08 11:12:46 +0200

    Finish AddProjectTask action, handle errors part3

--- ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import {addProjectTask} from "../../../actions/backlogActions";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
+import classnames from "classnames";
 
 class AddProjectTask extends Component {
     constructor(props) {
@@ -20,6 +21,13 @@ class AddProjectTask extends Component {
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+    }
+
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
     }
 
     // on change
@@ -47,6 +55,7 @@ class AddProjectTask extends Component {
 
     render() {
         const {id} = this.props.match.params;
+        const {errors} = this.state;
 
         return (
             <div className="add-PBI">
@@ -62,12 +71,17 @@ class AddProjectTask extends Component {
                                 <div className="form-group">
                                     <input
                                         type="text"
-                                        className="form-control form-control-lg"
+                                        className={classnames("form-control form-control-lg", {
+                                            "is-invalid": errors.summary
+                                        })}
                                         name="summary"
                                         placeholder="Project Task summary"
                                         value={this.state.summary}
                                         onChange={this.onChange}
                                     />
+                                    {errors.summary && (
+                                        <div className="invalid-feedback">{errors.summary}</div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                   <textarea
@@ -130,10 +144,15 @@ class AddProjectTask extends Component {
 }
 
 AddProjectTask.propTypes = {
-    addProjectTask: PropTypes.func.isRequired
+    addProjectTask: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     {addProjectTask}
 )(AddProjectTask);
